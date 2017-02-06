0a8ed2577012e767033ab9798e731b081629d226
framar67 <framar67@s.kippaustin.org>
2017-02-06 12:01:11 -0500

    first commit

---
+++ models/model.rb
@@ -0,0 +1,11 @@
+def car_chooser(price, brand)
+  if price=="cheap" && brand=="Toyota"
+    "Toyota Yaris"
+  
+  elsif price=="mid-range"
+    "Subaru Impreza Wrx"
+    
+    elsif price=="expensive"
+      "Nissan Nismo GT-R"
+  end
+end
